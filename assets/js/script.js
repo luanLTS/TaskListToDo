a9bc1055e0a694873e154b4d86ac1e2f14df9de4
Luan Teixeira <luanlts@outlook.com>
2022-07-13 17:17:34 -0300

    feat: implementa localStorage para armzenar as tarefas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
-let ulToDo = document.querySelector('.to_do')
-let ulDone = document.querySelector('.done')
-let inputTask = document.querySelector('#newTask')
-let addButton = document.querySelector('.add')
+/**
 
-let currentTask = 1
-
-let newTask = `
-    <li>{taskInput}</li>
+ let ulToDo = document.querySelector('.to-do')
+ let ulDone = document.querySelector('.done')
+ let inputTask = document.querySelector('#newTask')
+ let addButton = document.querySelector('.add')
+ 
+ let currentTask = 1
+ 
+ let newTask = `
+ <li>{taskInput}</li>
 `
+ulToDo.parentElement.addEventListener("click", (elemt) => {
+    console.log("ola");
+    console.log(elemt);
+})
 
 addButton.addEventListener('click', () => {
     if ( inputTask.value != '' ) {
@@ -22,7 +28,7 @@ inputTask.addEventListener('keydown', (e) => {
 })
 
 const addTask = () => {
-    newTask = `<li onclick="removeTask(this)" id="${ currentTask }" >${ inputTask.value }</li>`
+    newTask = `<li id="${ currentTask }" >${ inputTask.value }</li>`
     inputTask.value = ''
     ulToDo.innerHTML += newTask
     currentTask++
@@ -30,5 +36,89 @@ const addTask = () => {
 
 const removeTask = (element) => {
     ulToDo.removeChild(element)
-    ulDone.innerHTML += `<li><strike>${element.textContent}</strike></li>`
+    ulDone.innerHTML += `<li onclick="undoTask(this)">${element.textContent}</li>`
 }
+
+const undoTask = (element) => {
+    console.log("clicou na tarefa concluida");
+}
+
+*/
+
+const todo = document.getElementById("todo");
+const done = document.getElementById("done");
+let inputTask = document.querySelector("#newTask");
+let addButton = document.querySelector(".add");
+const ls = localStorage;
+
+let taskList = [];
+
+addButton.addEventListener("click", () => {
+    if (inputTask.value != "") {
+        add(inputTask.value);
+        inputTask.value = ``;
+    }
+});
+
+inputTask.addEventListener("keydown", (e) => {
+    if (e.keyCode == 13 && inputTask.value != "") {
+        add(inputTask.value);
+        inputTask.value = ``;
+    }
+});
+
+const checkLocalStorage = () => {
+    if (localStorage.getItem("tasks") !== null) return true;
+    else {
+        localStorage.setItem("tasks", JSON.stringify([]));
+    }
+};
+
+const add = async (title) => {
+    let oldTasks = JSON.parse(ls.getItem("tasks"));
+    let newTask = {
+        title,
+        id: Date.now(),
+        complete: false,
+    };
+    let newTasks = [...oldTasks, newTask];
+    ls.setItem("tasks", JSON.stringify(newTasks));
+    taskList = [...taskList, newTask];
+    show();
+};
+const toggle = async (id) => {
+    let tasks = JSON.parse(ls.getItem("tasks"));
+    let indexTask = tasks.findIndex((elemt) => elemt.id === id);
+    let auxTask = tasks[indexTask];
+    auxTask.complete = !auxTask.complete;
+    tasks[indexTask] = auxTask;
+    ls.setItem("tasks", JSON.stringify(tasks));
+    taskList[indexTask] = auxTask;
+    show();
+};
+
+const show = async () => {
+    todo.innerHTML = "";
+    done.innerHTML = "";
+    taskList.forEach((task) => {
+        if (!task.complete) {
+            todo.innerHTML += `<li onclick="toggle(${task.id})">${task.title} <i id="remove" onclick="remove(${task.id})" class="fas fa-trash-alt"></i></li>`;
+        } else {
+            done.innerHTML += `<li onclick="toggle(${task.id})">${task.title} <i id="remove" onclick="remove(${task.id})" class="fas fa-trash-alt"></i></li>`;
+        }
+    });
+};
+
+const remove = (id) => {
+    let tasks = JSON.parse(ls.getItem("tasks"));
+    let newTasks = tasks.filter((task) => task.id !== id);
+    ls.setItem("tasks", JSON.stringify(newTasks));
+    taskList = newTasks;
+    show();
+};
+
+window.addEventListener("load", async () => {
+    console.log(checkLocalStorage());
+    taskList = JSON.parse(ls.getItem("tasks"));
+    show();
+});
